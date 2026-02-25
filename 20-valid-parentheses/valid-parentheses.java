6d45b5d621d136e16f1f2f4893d70565d7692294
Gabriel Paniagua <gabyboyxp@gmail.com>
2026-02-24 22:41:23 -0600

    Time: 6 ms (6.64%) | Memory: 43.6 MB (16.01%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,67 +1,36 @@
 class Solution {
     public boolean isValid(String s) {
         /*
-        NOTE AFTER PROBLEM: NOT OPTIMIZED SOLUTION, LOOKING TO COME BACK TO LATER TO CLEAN UP
+        Input: string of parentheses chars
+        Output: boolean if string is valid
+        string will contain at least one parenthesis
+        if string is odd in length -> cannot be valid
 
-        Input: string consisting of only parathesis type chars
-        Output: boolean stating whether is valid
-        3 things:
-        -open bracket must be closed by same type -> ( )
-        -open brackets must be closed within a valid sequence -> [(])
-        -every close has a corresponding open same type
-        ([{[}]])
-
-        -Stack -> start of string 
-        -if a para of diff type is added, it must be the start of a para sequence
-        -if one of the same type as top of stack, consider that closed -> remove from stack 2 values
-        -any closing type of parathesis added to stack must match type of para at top of stack
+        -Stack structure, iterate through string
+        -if open parathesis add to the stack
+        -if close parenthesis -> does it equal the top of the stack
+        -if yes remove the corresponding open parenthesis (top of stack)
+        -if not -> string is not valid
         */
-        if (s.length() % 2 != 0) {
-            return false;
-        }
-
-        Deque<Character> parenthesesStack = new ArrayDeque<>();
-        int opening = 0;
-        int closing = 0;
+        Deque<Character> parentheses = new ArrayDeque<>();
+        Map<Character, Character> validParentheses = new HashMap<>();
+        validParentheses.put(')', '(');
+        validParentheses.put('}', '{');
+        validParentheses.put(']', '[');
 
         for (int i = 0; i < s.length(); i++) {
-            boolean validSoFar = true;
-            boolean foundClosing = false;
-            char c = s.charAt(i);
-            char charAtTop = c;
-            if (!parenthesesStack.isEmpty()) {
-                charAtTop = parenthesesStack.peekFirst();
+            if (!validParentheses.containsKey(s.charAt(i))) {
+                parentheses.push(s.charAt(i));
+                continue;
             }
-            if (c == ')' && parenthesesStack.peekFirst() != null) {
-                validSoFar = charAtTop == '(';
-                parenthesesStack.removeFirst();
-                foundClosing = true;
-                closing++;
+            if (!parentheses.isEmpty() && parentheses.peek() == validParentheses.get(s.charAt(i))) {
+                parentheses.pop();
             }
-            if (c == '}' && parenthesesStack.peekFirst() != null) {
-                validSoFar = charAtTop == '{';
-                parenthesesStack.removeFirst();
-                foundClosing = true;
-                closing++;
-            }
-            if (c == ']' && parenthesesStack.peekFirst() != null) {
-                validSoFar = charAtTop == '[';
-                parenthesesStack.removeFirst();
-                foundClosing = true;
-                closing++;
-            }
-            if (!validSoFar) {
+            else  {
                 return false;
             }
-            if (!foundClosing) {
-                parenthesesStack.addFirst(c);
-                opening++;
-            }
-        }
-        if (opening != closing) {
-            return false;
         }
 
-        return true;
+        return parentheses.isEmpty();
     }
 }
