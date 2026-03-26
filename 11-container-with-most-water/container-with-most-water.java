4d1ea9d9adacc27c8f23ce228aadce18eaca25e1
Gabriel Paniagua <gabyboyxp@gmail.com>
2026-03-25 23:00:54 -0500

    Time: 5 ms (82.63%) | Memory: 77.3 MB (80.12%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,31 +1,31 @@
 class Solution {
-    public int maxArea(int[] heights) {
-        /*
-        Input: array of integers representing heights where each value is the height of a bar and index is its location
-        Output: the max amount of water that can be stored in a container(area)
-        There will be at least two height
-        All height values will either be zero or positive
+    public int maxArea(int[] arr) {
+    /*
+    Input: integer array where element represents height of vertical line at index
+    Output: maximum amount of water that a container can hold
+    Edge case: must be done with two different lines
+    Ex. [1, 2, 5, 3]
+               ^  ^
+    -Container is limited by the smaller value since going over would cause overflow
 
-        Brute force:Calculating every single possibility -> O(n^2)
+    Approach:
+    - Window with two pointers to move our optimal areas
+    - Moving inwards is guranteed to give a smaller value if heights stayed the same -> we must
+    find a height that increases our area
+    - Limited by the smaller value, so move our window inwards depending on smallest
+     */
 
-        [1, 6, 3, 9]
-        -Area is limited by the smaller height
-        -Start at the end of the list to start by maximizing the length attribute
-        */
+        int left = 0, right = arr.length - 1, maxArea = 0;
 
-        int leftPointer = 0;
-        int rightPointer = heights.length - 1;
-        int maxArea = 0;
+        while (left < right) {
+            int limitingHeight = Math.min(arr[left], arr[right]);
+            maxArea = Math.max(maxArea, limitingHeight * (right - left));
 
-        while (leftPointer < rightPointer) {
-            int length = rightPointer - leftPointer;
-            int height = Math.min(heights[leftPointer], heights[rightPointer]);
-            maxArea = Math.max(height*length, maxArea);
-            if (heights[leftPointer] > heights[rightPointer]) {
-                rightPointer--;
+            if (arr[left] > arr[right]) {
+                right--;
             }
             else {
-                leftPointer++;
+                left++;
             }
         }
 
